07277ae319b7e18142a614eb5c72e5e5a4c63dd7
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-11-16 20:55:06 +0600

    create a simple navbar

--- src/pages/Shared/Navigation/Navbar/Navbar.jsx
+++ src/pages/Shared/Navigation/Navbar/Navbar.jsx
@@ -1,4 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
 import Container from "../../../Responsive/Container";
+import ThemeToggle from "../../../../Components/Theme/ToggleTheme";
+
+import { TbTruckDelivery } from "react-icons/tb";
+import { FaBars } from "react-icons/fa";
+
+const MenuLink = (
+  <>
+    <div className="flex flex-col  lg:flex-row gap-3 md:gap-6  items-center justify-center font-medium capitalize text-base ">
+      <NavLink to={"/service"}>service</NavLink>
+      <NavLink to={"/coverage"}>Coverage</NavLink>
+      <NavLink to={"/about"}>about</NavLink>
+      <NavLink to={"/service"}>pricing</NavLink>
+      <NavLink to={"/blog"}>blog</NavLink>
+      <NavLink to={"/contact"}>contact</NavLink>
+    </div>
+  </>
+);
 
 const Navbar = () => {
   return (
@@ -11,70 +29,38 @@ const Navbar = () => {
                 tabIndex={0}
                 role="button"
                 className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  {" "}
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />{" "}
-                </svg>
+                <FaBars size={20} />
               </div>
               <ul
                 tabIndex="-1"
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
+                {MenuLink}
               </ul>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+            <a className="font-bold text-xl flex gap-1 justify-center items-center ">
+              <span className="hidden sm:flex">
+                <TbTruckDelivery size={30} />
+              </span>{" "}
+              Go Deliver
+            </a>
           </div>
           <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+            <ul className="menu menu-horizontal px-1">{MenuLink}</ul>
           </div>
-          <div className="navbar-end">
-            <a className="btn">Button</a>
+          <div className="navbar-end ">
+            <div className="flex items-center space-x-10">
+              <div>
+                <ThemeToggle />
+              </div>
+              <div className="space-x-3 hidden md:flex">
+                <Link to={"/login"} className="btn">
+                  Signin
+                </Link>
+                <Link to={"/be-a-order"} className="btn">
+                  Be a rider
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
