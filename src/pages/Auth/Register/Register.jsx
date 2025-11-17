1900904e06982f03082f099ebe60c29612b82c4a
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-11-17 19:48:51 +0600

    impliment image and name and login and resister

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -17,6 +17,35 @@ const Register = () => {
         <div className="card bg-base-100 w-full  max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <fieldset className="fieldset">
+              {/* Name field */}
+              <label className="label">Name</label>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                className="input "
+                placeholder="your name"
+              />
+
+              {errors.email?.type === "required" && (
+                <>
+                  <p className="text-red-500">Name is required</p>
+                </>
+              )}
+              {/* Photo Image field */}
+              <label className="label">Image</label>
+              <input
+                type="file"
+                {...register("Image", { required: true })}
+                className="file-input "
+                placeholder="your photo image"
+              />
+
+              {errors.email?.type === "required" && (
+                <>
+                  <p className="text-red-500">photo is required</p>
+                </>
+              )}
+
               {/* email field */}
               <label className="label">Email</label>
               <input
@@ -31,17 +60,38 @@ const Register = () => {
                   <p className="text-red-500">Email is required</p>
                 </>
               )}
-              {/* password  field */}
 
+              {/* password  field */}
               <label className="label">Password</label>
               <input
+                className="input"
                 type="password"
-                {...register("password", { required: true })}
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  pattern:
+                    /^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[A-Z])(?=.*[a-z]).{8,}$/,
+                })}
                 placeholder="Password"
               />
               {errors.password?.type === "required" && (
                 <>
                   <p className="text-red-500">Password is required</p>
+                </>
+              )}
+              {errors.password?.type === "minLength" && (
+                <>
+                  <p className="text-red-500">
+                    Password most be 6 characters or longer
+                  </p>
+                </>
+              )}
+              {errors.password?.type === "pattern" && (
+                <>
+                  <p className="text-red-500">
+                    Password most including uppercase and lowercase and one
+                    number
+                  </p>
                 </>
               )}
               <div>
